c5371d9231adde12e2bccb9d098597d25018c7c1
NR-BIT <95742365+NR-BIT@users.noreply.github.com>
2022-02-24 12:52:14 +0100

    Start3

---
+++ classes/classes.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Wenn du etwas in php machst dann immer in den <?php ?> task 
+ */
+
+ class classes {
+     /**
+      * So definierst du eine statische funktion die du überall aufrufen kannst
+      * aufruf mit classes::codeexample(); dafür muss am anfang der datei include_once 'includes.php'; geschrieben werden :) 
+      * passt?
+      */
+     public static function codeexample () {
+        /**
+         * um hier html code rein zu schreiben einfach php tag closen und wieder öffnen
+         */
+        ?>
+            <!-- Hier dein HTML Code der Wiederholt gebraucht wird Wenn was ist schreib einfach GL-->
+        <?php
+     }
+ }
